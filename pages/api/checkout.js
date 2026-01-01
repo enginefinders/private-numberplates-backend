9c178cadea1febd7d55ac9ae879a223d3562ff53
enginefinders <enginefinders5@gmail.com>
2026-01-01 22:00:28 +0500

    update fixed

--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -3,8 +3,8 @@ import axios from "axios";
 import { Resend } from 'resend';
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Method not allowed" });
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
   }
 
   try {
