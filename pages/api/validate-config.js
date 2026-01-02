d3e05dd11e0780653474c22c2d2239ef7660d928
enginefinders <enginefinders5@gmail.com>
2026-01-02 14:07:39 +0500

    update

--- pages/api/validate-config.js
+++ pages/api/validate-config.js
@@ -1,9 +1,16 @@
 // pages/api/validate-config.js
 import { normalizePlateConfig } from '../../lib/plateSchema'; // fixed relative path
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+export default async function handler(req, res) {
+
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-export default async function handler(req, res) {
+
+    if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
