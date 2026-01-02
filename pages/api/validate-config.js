984f03f7fa342bd37f78346a59326291b28987c4
enginefinders <enginefinders5@gmail.com>
2026-01-02 14:41:12 +0500

    update

--- pages/api/validate-config.js
+++ pages/api/validate-config.js
@@ -3,7 +3,7 @@ import { normalizePlateConfig } from '../../lib/plateSchema'; // fixed relative 
 
 export default async function handler(req, res) {
 
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.private-numberplates.uk');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
